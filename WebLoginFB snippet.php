6189ac18303a03d658fc3f85d2efa5887dd051a0
Brian Senior <brian@greenhatdesign.net>
2011-06-07 16:12:19 +0100

    Initial work on generic Modx sign in page which includes Facebook Connect

---
+++ WebLoginFB snippet.php
@@ -0,0 +1,207 @@
+<?php
+# Created By Raymond Irving 2004
+# Facebook parts added by B A Senior 2011
+#::::::::::::::::::::::::::::::::::::::::
+# Params:	
+#
+#	&loginhomeid 	- (Optional)
+#		redirects the user to first authorized page in the list.
+#		If no id was specified then the login home page id or 
+#		the current document id will be used
+#
+#	&logouthomeid 	- (Optional)
+#		document id to load when user logs out	
+#
+#	&pwdreqid 	- (Optional)
+#		document id to load after the user has submited
+#		a request for a new password
+#
+#	&pwdactid 	- (Optional)
+#		document id to load when the after the user has activated
+#		their new password
+#
+#	&logintext		- (Optional) 
+#		Text to be displayed inside login button (for built-in form)
+#
+#	&logouttext 	- (Optional)
+#		Text to be displayed inside logout link (for built-in form)
+#	
+#	&tpl			- (Optional)
+#		Chunk name or document id to as a template
+#				  
+#	Note: Templats design:
+#			section 1: login template
+#			section 2: logout template 
+#			section 3: password reminder template 
+#
+#			See weblogin.tpl for more information
+#
+# Examples:
+#
+#	[[WebLogin? &loginhomeid=`8` &logouthomeid=`1`]] 
+#
+#	[[WebLogin? &loginhomeid=`8,18,7,5` &tpl=`Login`]] 
+
+//initialise variable
+$output="";
+
+# Set Snippet Paths 
+$snipPath = $modx->config['base_path'] . "assets/snippets/";
+
+# check if inside manager
+if ($m = $modx->insideManager()) {
+	return ''; # don't go any further when inside manager
+}
+
+# deprecated params - only for backward compatibility
+if(isset($loginid)) $loginhomeid=$loginid;
+if(isset($logoutid)) $logouthomeid = $logoutid;
+if(isset($template)) $tpl = $template;
+
+# Snippet customize settings
+$liHomeId	= isset($loginhomeid)? explode(",",$loginhomeid):array($modx->config['login_home'],$modx->documentIdentifier);
+$loHomeId	= isset($logouthomeid)? $logouthomeid:$modx->documentIdentifier;
+$pwdReqId	= isset($pwdreqid)? $pwdreqid:0;
+$pwdActId	= isset($pwdactid)? $pwdactid:0;
+$loginText	= isset($logintext)? $logintext:'Sign in';
+$logoutText	= isset($logouttext)? $logouttext:'Sign out';
+$tpl		= isset($tpl)? $tpl:"";
+
+# System settings
+$webLoginMode = isset($_REQUEST['webloginmode'])? $_REQUEST['webloginmode']: '';
+$isLogOut		= $webLoginMode=='lo' ? 1:0;
+$isPWDActivate	= $webLoginMode=='actp' ? 1:0;
+$isPostBack		= count($_POST) && (isset($_POST['cmdweblogin']) || isset($_POST['cmdweblogin_x']));
+$txtPwdRem 		= isset($_REQUEST['txtpwdrem'])? $_REQUEST['txtpwdrem']: 0;
+$isPWDReminder	= $isPostBack && $txtPwdRem=='1' ? 1:0;
+
+$site_id = isset($site_id)? $site_id: '';
+$cookieKey = substr(md5($site_id."Web-User"),0,15);
+
+# Start processing
+include_once $snipPath."weblogin/weblogin.common.inc.php";
+include_once ($modx->config['base_path'] . "manager/includes/crypt.class.inc.php");
+include_once $snipPath."weblogin/login.function.inc.php";
+
+
+if ($isPWDActivate || $isPWDReminder || $isLogOut || $isPostBack) {
+	# include the logger class
+	include_once $modx->config['base_path'] . "manager/includes/log.class.inc.php";
+	include_once $snipPath."weblogin/weblogin.processor.inc.php";
+}
+
+    //run weblogin as usual - comment this out if you don't want the old style Modx login option
+	include_once $snipPath."weblogin/weblogin.inc.php";
+
+//name of the modx user group
+$usergroup="WebUsers";
+$passwordpostfix="ibank";  //make this change in the login function also
+$email="pleaseenter@youremail.here";
+
+//database variables
+$table_prefix = $modx->dbConfig['table_prefix'];
+
+//if logged in then send to home page
+if ($modx->getLoginUserType() == 'web')
+{
+    $modx->sendForward(1);
+}else
+{
+    include_once ('./facebook/facebook.php');
+    // Create our Application instance (replace this with your appId and secret).
+    $facebook = new Facebook(array(
+      'appId'  => '[YOUR_APP_ID]',
+      'secret' => '[YOUR_FB_SECRET]',
+      'cookie' => true,
+    ));
+    // If we get a session here, it means we found a correctly signed session using
+    // the Application Secret only Facebook and the Application know. We dont know
+    // if it is still valid until we make an API call using the session. A session
+    // can become invalid if it has already expired (should not be getting the
+    // session back in this case) or if the user logged out of Facebook.
+    $session = $facebook->getSession();
+    
+    $me = null;
+    // Session based API call.
+    if ($session)
+    {
+      try
+      {
+        $user_id = $facebook->getUser();
+        $me = $facebook->api('/me');
+        /* This for debug - don't forget to add a return $output */
+        //DEBUG   $output.="user ID: ".$user_id;
+        //DEBUG $output.="<img src=\"https://graph.facebook.com/".$user_id."/picture\">";
+        $fullname=$me['name'];
+        //DEBUG $output.="name:".$fullname;
+        //create the modx username, password and add to DB
+        $username=$user_id;
+        $pass = md5($user_id.$passwordpostfix);
+        //does the record exist?
+        $result = $modx->db->select('username',$modx->getFullTableName('web_users')," username = '".$user_id."'");
+        if($modx->db->getRecordCount($result) != 1)
+        {
+            //no it doesn't - so create the user account
+            //DEBUG $output.="user ".$user_id." doesn't exist in the database";
+            $sql = "INSERT INTO ".$modx->getFullTableName('web_users')." (username,password) VALUES ('".$user_id."', '".$pass."')";
+            $rs = $modx->db->query($sql);
+            if(!$rs)
+            {
+                $output.="<p>ERROR: unable to write to database table ".$modx->getFullTableName('web_users');
+                $output.="<br /> Please report this error to the site admin</p>";
+            }else
+            {
+                $uid = $modx->db->getInsertId();
+                //DEBUG $output.="<br />UID: ".$uid." Fullname: ".$fullname." email: ".$email;
+                //$sql = "INSERT INTO ".$modx->getFullTableName('web_user_attributes')." (internalKey,fullname,email) VALUES (".$uid.",'".$fullname"','".$email."');";
+                $sql = "INSERT INTO ".$modx->getFullTableName('web_user_attributes')." (internalKey,fullname,email) VALUES (".$uid.",'".$fullname."','".$email."');";
+                //DEBUG $output.="<br />SQL: ".$sql;
+                
+                   $rs = $modx->db->query($sql);
+                   if(!$rs) {
+                              $modx->db->delete($modx->getFullTableName('web_users'),"id = ".$uid);
+                              $output.="<p>ERROR: unable to write to database table ".$modx->getFullTableName('web_user_attributes');
+                              $output.="<br /> Please report this error to the site admin</p>";
+                    } else
+                    {
+                        // add user to the group
+                              $result = $modx->db->select('id',$modx->getFullTableName('webgroup_names'),"name = '".$usergroup."'");
+                              if($modx->db->getRecordCount($result) == 1) {
+                                        $gid = $modx->db->getValue($result);
+                                        $sql = "INSERT INTO ".$modx->getFullTableName('web_groups')." (webgroup, webuser) VALUES (".$gid.",".$uid.")";
+                                        $result = $modx->db->query($sql);
+                                        if (!$result)
+                                        {
+                                            $output.="<p>ERROR: unable to write to database table ".$modx->getFullTableName('web_groups');
+                                            $output.="<br /> Please report this error to the site admin</p>";
+                                        }else
+                                        {
+                                          //DEBUG  $output.="<br />All data written to DB ok";
+                                        }
+                               }else
+                               {
+                                    $output.="<p>ERROR: unable to find user group ".$usergroup." in database table ".$modx->getFullTableName('web_groups');
+                                    $output.="<br /> Please report this error to the site admin</p>";
+                               }
+                    }
+            }       
+        }
+        
+      } catch (FacebookApiException $e)
+      {
+        error_log($e);
+        //DEBUG $output.="Error: ".$e;
+      }
+      login($user_id);   
+
+    }
+    
+//if logged in then send to home page
+if ($modx->getLoginUserType() == 'web')
+{
+    $modx->sendForward(1);
+} 
+
+return $output;
+}
+?>
---
+++ login.function.inc.php
@@ -0,0 +1,238 @@
+<?php
+function login($user) {
+# process login - this function from http://modxcms.com/forums/index.php?topic=32390.20
+
+# Start processing
+
+/* add in required WebAuth etc functions */
+# Set Snippet Paths 
+$snipPath = $modx->config['base_path'] . "assets/snippets/";
+include_once $snipPath."weblogin/weblogin.common.inc.php";
+include_once ($modx->config['base_path'] . "manager/includes/crypt.class.inc.php");
+    global $modx;
+
+defined('IN_PARSER_MODE') or die();
+
+$dbase = $modx->dbConfig['dbase'];
+$table_prefix = $modx->dbConfig['table_prefix'];
+$passwordpostfix="ibank"; //make tis change in the WebLoginFB snippet also
+    $logindetails = array();
+    $logindetails['username'] = $user;
+    $logindetails['givenPass'] = $user.$passwordpostfix; //needs to match that set in the main body
+    $rememberme=FALSE;
+
+    $username = $modx->db->escape(strip_tags($logindetails['username']));
+    $givenPassword = $modx->db->escape($logindetails['givenPass']);
+
+    // invoke OnBeforeWebLogin event
+    $modx->invokeEvent("OnBeforeWebLogin",
+                            array(
+                                "username"        => $username,
+                                "userpassword"    => $givenPassword,
+                                "rememberme"    => $rememberme
+                            ));
+
+    $sql = "SELECT $dbase.`".$table_prefix."web_users`.*, $dbase.`".$table_prefix."web_user_attributes`.* FROM $dbase.`".$table_prefix."web_users`, $dbase.`".$table_prefix."web_user_attributes` WHERE BINARY $dbase.`".$table_prefix."web_users`.username = '".$username."' and $dbase.`".$table_prefix."web_user_attributes`.internalKey=$dbase.`".$table_prefix."web_users`.id;";
+    $ds = $modx->db->query($sql);
+    $limit = $modx->db->getRecordCount($ds);
+
+    if($limit==0 || $limit>1) {
+        $output = webLoginAlert("Incorrect username or password entered!");
+        return;
+    }
+
+    $row = $modx->db->getRow($ds);
+
+    $internalKey             = $row['internalKey'];
+    $dbasePassword             = $row['password'];
+    $failedlogins             = $row['failedlogincount'];
+    $blocked                 = $row['blocked'];
+    $blockeduntildate        = $row['blockeduntil'];
+    $blockedafterdate        = $row['blockedafter'];
+    $registeredsessionid    = $row['sessionid'];
+    $role                    = $row['role'];
+    $lastlogin                = $row['lastlogin'];
+    $nrlogins                = $row['logincount'];
+    $fullname                = $row['fullname'];
+    //$sessionRegistered         = checkSession();
+    $email                     = $row['email'];
+
+    // load user settings
+    if($internalKey){
+        $result = $modx->db->query("SELECT setting_name, setting_value FROM ".$dbase.".`".$table_prefix."web_user_settings` WHERE webuser='$internalKey'");
+        while ($row = $modx->fetchRow($result, 'both')) $modx->config[$row[0]] = $row[1];
+    }
+
+    if($failedlogins>=$modx->config['failed_login_attempts'] && $blockeduntildate>time()) {    // blocked due to number of login errors.
+        session_destroy();
+        session_unset();
+        $output = webLoginAlert("Due to too many failed logins, you have been blocked!");
+        return;
+    }
+
+    if($failedlogins>=$modx->config['failed_login_attempts'] && $blockeduntildate<time()) {    // blocked due to number of login errors, but get to try again
+        $sql = "UPDATE $dbase.`".$table_prefix."web_user_attributes` SET failedlogincount='0', blockeduntil='".(time()-1)."' where internalKey=$internalKey";
+        $ds = $modx->db->query($sql);
+    }
+
+    if($blocked=="1") { // this user has been blocked by an admin, so no way he's loggin in!
+        session_destroy();
+        session_unset();
+        $output = webLoginAlert("You are blocked and cannot log in!");
+        return;
+    }
+
+    // blockuntil
+    if($blockeduntildate>time()) { // this user has a block until date
+        session_destroy();
+        session_unset();
+        $output = webLoginAlert("You are blocked and cannot log in! Please try again later.");
+        return;
+    }
+
+    // blockafter
+    if($blockedafterdate>0 && $blockedafterdate<time()) { // this user has a block after date
+        session_destroy();
+        session_unset();
+        $output = webLoginAlert("You are blocked and cannot log in! Please try again later.");
+        return;
+    }
+
+    // allowed ip
+    if (isset($modx->config['allowed_ip'])) {
+        if (strpos($modx->config['allowed_ip'],$_SERVER['REMOTE_ADDR'])===false) {
+            $output = webLoginAlert("You are not allowed to login from this location.");
+            return;
+        }
+    }
+
+    // allowed days
+    if (isset($modx->config['allowed_days'])) {
+        $date = getdate();
+        $day = $date['wday']+1;
+        if (strpos($modx->config['allowed_days'],"$day")===false) {
+            $output = webLoginAlert("You are not allowed to login at this time. Please try again later.");
+            return;
+        }
+    }
+
+    // invoke OnWebAuthentication event
+    $rt = $modx->invokeEvent("OnWebAuthentication",
+                            array(
+                                "userid"        => $internalKey,
+                                "username"      => $username,
+                                "userpassword"  => $givenPassword,
+                                "savedpassword" => $dbasePassword,
+                                "rememberme"    => $rememberme
+                            ));
+    // check if plugin authenticated the user
+
+    if (!$rt||(is_array($rt) && !in_array(TRUE,$rt))) {
+        // check user password - local authentication
+        if($dbasePassword != md5($givenPassword)) {
+            $output = webLoginAlert("Incorrect username or password entered!");
+            $newloginerror = 1;
+        }
+    }
+
+    if(isset($modx->config['use_captcha']) && $modx->config['use_captcha']==1) {
+        if($_SESSION['veriword']!=$captcha_code) {
+            $output = webLoginAlert("The security code you entered didn't validate! Please try to login again!");
+            $newloginerror = 1;
+        }
+    }
+
+    if(isset($newloginerror) && $newloginerror==1) {
+        $failedlogins += $newloginerror;
+        if($failedlogins>=$modx->config['failed_login_attempts']) { //increment the failed login counter, and block!
+            $sql = "update $dbase.`".$table_prefix."web_user_attributes` SET failedlogincount='$failedlogins', blockeduntil='".(time()+($modx->config['blocked_minutes']*60))."' where internalKey=$internalKey";
+            $ds = $modx->db->query($sql);
+        } else { //increment the failed login counter
+            $sql = "update $dbase.`".$table_prefix."web_user_attributes` SET failedlogincount='$failedlogins' where internalKey=$internalKey";
+            $ds = $modx->db->query($sql);
+        }
+        session_destroy();
+        session_unset();
+        return;
+    }
+
+    $currentsessionid = session_id();
+
+    if(!isset($_SESSION['webValidated'])) {
+        $sql = "update $dbase.`".$table_prefix."web_user_attributes` SET failedlogincount=0, logincount=logincount+1, lastlogin=thislogin, thislogin=".time().", sessionid='$currentsessionid' where internalKey=$internalKey";
+        $ds = $modx->db->query($sql);
+    }
+
+    $_SESSION['webShortname']=$username;
+    $_SESSION['webFullname']=$fullname;
+    $_SESSION['webEmail']=$email;
+    $_SESSION['webValidated']=1;
+    $_SESSION['webInternalKey']=$internalKey;
+    $_SESSION['webValid']=base64_encode($givenPassword);
+    $_SESSION['webUser']=base64_encode($username);
+    $_SESSION['webFailedlogins']=$failedlogins;
+    $_SESSION['webLastlogin']=$lastlogin;
+    $_SESSION['webnrlogins']=$nrlogins;
+    $_SESSION['webUserGroupNames'] = ''; // reset user group names
+
+    // get user's document groups
+    $dg='';$i=0;
+    $tblug = $dbase.".`".$table_prefix."web_groups`";
+    $tbluga = $dbase.".`".$table_prefix."webgroup_access`";
+    $sql = "SELECT uga.documentgroup
+            FROM $tblug ug
+            INNER JOIN $tbluga uga ON uga.webgroup=ug.webgroup
+            WHERE ug.webuser =".$internalKey;
+    $ds = $modx->db->query($sql);
+    while ($row = $modx->db->getRow($ds,'num')) $dg[$i++]=$row[0];
+    $_SESSION['webDocgroups'] = $dg;
+
+    if($rememberme) {
+        $_SESSION['modx.web.session.cookie.lifetime']= intval($modx->config['session.cookie.lifetime']);
+    } else {
+        $_SESSION['modx.web.session.cookie.lifetime']= 0;
+    }
+    // update active users list if redirectinq to another page
+    if($id!=$modx->documentIdentifier) {
+        if (getenv("HTTP_CLIENT_IP")) $ip = getenv("HTTP_CLIENT_IP");else if(getenv("HTTP_X_FORWARDED_FOR")) $ip = getenv("HTTP_X_FORWARDED_FOR");else if(getenv("REMOTE_ADDR")) $ip = getenv("REMOTE_ADDR");else $ip = "UNKNOWN";$_SESSION['ip'] = $ip;
+        $itemid = isset($_REQUEST['id']) ? $_REQUEST['id'] : 'NULL' ;$lasthittime = time();$a = 998;
+
+        if($a!=1) {
+            // web users are stored with negative id
+            $sql = "REPLACE INTO $dbase.`".$table_prefix."active_users` (internalKey, username, lasthit, action, id, ip) values(-".$_SESSION['webInternalKey'].", '".$_SESSION['webShortname']."', '".$lasthittime."', '".$a."', ".$itemid.", '$ip')";
+            if(!$ds = $modx->db->query($sql)) {
+                $output = "error replacing into active users! SQL: ".$sql;
+                return;
+            }
+        }
+    }
+
+    // invoke OnWebLogin event
+    $modx->invokeEvent("OnWebLogin",
+                            array(
+                                "userid"        => $internalKey,
+                                "username"        => $username,
+                                "userpassword"    => $givenPassword,
+                                "rememberme"    => $rememberme
+                            ));
+
+    // redirect
+    if(isset($_REQUEST['refurl']) && !empty($_REQUEST['refurl'])) {
+        // last accessed page
+        $targetPageId= urldecode($_REQUEST['refurl']);
+        if (strpos($targetPageId, 'q=') !== false) {
+            $urlPos = strpos($targetPageId, 'q=')+2;
+            $alias = substr($targetPageId, $urlPos);
+            $aliasLength = (strpos($alias, '&'))? strpos($alias, '&'): strlen($alias);
+            $alias = substr($alias, 0, $aliasLength);
+            $url = $modx->config['base_url'] . $alias;
+        } elseif (intval($targetPageId)) {
+            $url = $modx->makeUrl($targetPageId);
+        } else {
+            $url = urldecode($_REQUEST['refurl']);
+        }
+        $modx->sendRedirect($url);
+    }
+    return;
+}
+?>
